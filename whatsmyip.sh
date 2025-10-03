2ddb0a5755c5bc6bf3d7645c97ad8b2312aa9a8a
jason@beast <kindofabuzz@gmail.com>
2025-10-02 21:24:21 -0500

    first commit

---
+++ whatsmyip.sh
@@ -0,0 +1,18 @@
+alias whatismyip="whatsmyip"
+function whatsmyip () {
+    echo -e "\n\e[33m@@@@@@@@@@@@@@@@@@@@@@@@@@@@\e[0m"
+    # Internal IP Lookup.
+    if command -v ip &> /dev/null; then
+        echo -n -e "\e[32mInternal IP: \e[0m"
+        ip addr show enp7s0 | grep "inet " | awk '{print $2}' | cut -d/ -f1
+    else
+        echo -n "Internal IP: "
+        ifconfig enp7s0 | grep "inet " | awk '{print $2}'
+    fi
+
+    # External IP Lookup
+    echo -n -e "\e[32mExternal IP: \e[0m"
+    curl -4 ifconfig.me
+    echo -e "\n\e[33m@@@@@@@@@@@@@@@@@@@@@@@@@@@@\e[0m"
+
+}
